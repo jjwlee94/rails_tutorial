88f0573d9d79adfcbab2e66550c28e0ab5ffada8
Jeewon Lee <jeewon.l@getjobber.com>
2022-06-01 15:34:34 -0700

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
+      reset_session
+      log_in @user
       flash[:success] = 'Welcome to the Sample App!'
       redirect_to @user
     else
